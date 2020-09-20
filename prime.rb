2489fa9bb81fffa72efe7eee45b0606a70f8881a
John Troutman <jtroutmanman@gmail.com>
2020-09-20 20:02:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 # Add  code here!
 def prime?(num)
+  return false if num < 1
   (2..num - 1).each do |int|
-    return false if num % int == 0 || num < 1
+    return false if num % int == 0
   end
   true
 end
