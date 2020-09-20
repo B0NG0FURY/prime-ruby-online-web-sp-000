6127144be76d0fa4cd6bc9875ac3b5b11fbe4525
John Troutman <jtroutmanman@gmail.com>
2020-09-20 19:51:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,6 @@
 # Add  code here!
 def prime?(num)
-  if num <= 1
-    false
-  elsif num == 2 || num == 3
-    true
-  else
-    num_array = (1..(num - 1)).to_a
+    num_array = (2..(num - 1)).to_a
     num_array.each do |int|
       if num % int == 0
         false
