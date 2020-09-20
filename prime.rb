bd6e6aaedf1c107fe18bd7c10ad0348836dcf56e
John Troutman <jtroutmanman@gmail.com>
2020-09-20 19:41:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,17 @@
 # Add  code here!
 def prime?(num)
-  num_array = (2..(num - 1)).to_a
-  num_array.each do |int|
-    if num % int == 0
-      false
+  if num <= 1
+    false
+  end
+  if num == 2 || num == 3
+    true
+  else
+    num_array = (2..(num - 1)).to_a
+    num_array.each do |int|
+      if num % int == 0
+        false
     end
   end
+  end
+  true
 end
